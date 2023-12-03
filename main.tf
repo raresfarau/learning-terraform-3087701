cac6f67c0c8e8d6f0790791c0838bc6c323cc65c
Rares Farau <126510029+raresfarau@users.noreply.github.com>
2023-12-03 23:20:14 +0200

    Initial commit 2

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "variable.instance_type"
+  instance_type = "aws_instance"
 
   tags = {
     Name = "HelloWorld"
