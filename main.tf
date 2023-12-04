a91ec12ce47801d56ae3068818f58a5a4354a9e5
Rares Farau <126510029+raresfarau@users.noreply.github.com>
2023-12-04 15:13:41 +0200

    Use module for security group

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "blog_sg" {
   version = "4.13.0"
 
   vpc_id  = data.aws_vpc.default.id
-  name    = "blog_new"
+  name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
